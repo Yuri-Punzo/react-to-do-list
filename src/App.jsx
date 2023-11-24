3d5c0a33c97404733af08fe8361c5b4a9e378c9a
Yuri-Punzo <yuripunzo@hotmail.it>
2023-11-24 18:07:16 +0100

    added counter with 2 buttons & relative functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import logo from './logo.svg';
 import './App.scss';
 import Component from './components/component';
+import { useState } from 'react';
 
 function App() {
+  let Uno = 1;
+  // cerca useState e questa variabile
+  let [count, setCount] = useState(0);
+  function increment() {
+    setCount(count + 1)
+  };
+  function decrement() {
+    setCount(count - 1)
+  };
   return (
     <div className="App">
       <header className="App-header">
@@ -10,11 +20,13 @@ function App() {
         <h1 className='mb-5 mt-2'>
           Prova Prova SA SA
         </h1>
+
         <Component name='Yuri' />
+
         <div className="container text-center">
           <div className="row">
             <div className="col">
-              1
+              {Uno}
             </div>
             <div className="col bg-primary">
               {3 - 1 /* tutto quello che é fra parentesi graffe viene letto come JS */}
@@ -23,6 +35,13 @@ function App() {
               3
             </div>
           </div>
+        </div>
+        <div>
+          <div>
+            {count}
+          </div>
+          <button onClick={increment}>+</button>
+          <button onClick={decrement}>-</button>
         </div>
         {/* <p>
           Edit <code>src/App.js</code> and save to reload.
